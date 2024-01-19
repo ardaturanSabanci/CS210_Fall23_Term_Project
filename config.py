3603c55f95f1f6df0ab7c5a627e81fe0bf7a0919
ardaturanSabanci <156612131+ardaturanSabanci@users.noreply.github.com>
2024-01-19 13:32:35 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-#insert local parameters
-
-username = 'your-spotify-username'
-client_id ='your-client-id'
-client_secret = 'your-client-secret'
-redirect_uri = 'http://localhost:7777/callback'
-scope = 'user-read-recently-played'
